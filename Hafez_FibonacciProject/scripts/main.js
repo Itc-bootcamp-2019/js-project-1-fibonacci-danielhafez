9fb5952a87c3f811d0247d98a46172b1d368d9a6
Daniel Hafez <Daniel@MacBook-Pro-di-Daniel-4.local>
2020-03-04 15:42:39 +0200

    not a new mileston but improved my code

--- Hafez_FibonacciProject/scripts/main.js
+++ Hafez_FibonacciProject/scripts/main.js
@@ -1,17 +1,65 @@
 function calculateFibonacci(num) {
-  if (num == 0) {
-    return 0;
+  if (isNaN(num)) {
+    alert("Insert numbers!");
+    return console.error(
+      "The classic Fibonacci Sequence includes only positive number, the input was not a number"
+    );
   }
-  if (num == 1) {
-    return 1;
+  if (num < 0) {
+    alert(
+      "The classic Fibonacci Sequence includes only positive number, the number inserted was negative"
+    );
+    return console.error(
+      "The classic Fibonacci Sequence includes only positive number, the number inserted was negative."
+    );
+  }
+  if (num <= 1) {
+    return num;
   } else {
-    return calculateFibonacci(n - 1) + calculateFibonacci(n - 2);
+    return calculateFibonacci(num - 1) + calculateFibonacci(num - 2);
   }
 }
 
 function calculation() {
   let x = document.getElementById("first_value").value;
-  console.log(x);
-  result = calculateFibonacci(x);
-  document.getElementById("result").innerText = result;
+  document.getElementById("result").innerText = calculateFibonacci(x);
 }
+
+let button = document.getElementById("calculate-btn");
+button.addEventListener("click", calculation);
+
+// // const url = "http://localhost:5050/fibonacci/:number";
+
+// fetch(``)
+//   .then(function(response) {
+//     if (response.status !== 200) {
+//       console.log(
+//         "Looks like there was a problem. Status Code: " + response.status
+//       );
+//       return;
+//     }
+//     response.json().then(function(data) {
+//       console.log(data);
+//     });
+//   })
+//   .catch(function(err) {
+//     console.log("Fetch Error :-S", err);
+//   });
+
+// function calculateFibonacci(num) {
+//   let elementOne = 1;
+//   let elementTwo = 0;
+//   let storage;
+
+//   if (num === 0) return 0;
+//   if (num === 1 || num === 2) return 1;
+//   else {
+//     for (num; num >= 1; num--) {
+//       storage = elementOne;
+//       elementOne = elementOne + elementTwo;
+//       elementTwo = storage;
+//     }
+
+//     return elementTwo;
+//   }
+// }
